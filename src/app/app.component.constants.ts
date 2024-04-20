27860f52117cef52e7f3f5094a7cad40b06b6121
edenknudson <edenlknudson@gmail.com>
2024-04-20 17:36:35 -0400

    added images

--- src/app/app.component.constants.ts
+++ src/app/app.component.constants.ts
@@ -5,7 +5,8 @@ export interface Event {
   details: string,
   panelOpenState: boolean,
   link: string,
-  index: number
+  index: number,
+  image: string
 }
 
 export const EventDetails: Event[] = [
@@ -16,7 +17,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'William A. Turner invented the poptart 1964. Initially called Fruit Scones, the name was soon changed to Pop-Tarts as a pun on the then popular Pop Art movement.',
     panelOpenState: false,
-    link: 'https://www.poptarts.com/en_US/our-story.html'
+    link: 'https://www.poptarts.com/en_US/our-story.html',
+    image: 'https://i.huffpost.com/gen/2112636/thumbs/o-1964-570.jpg?6'
   },
   {
     index: 0,
@@ -25,7 +27,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'In the summer of 1911, the American archaeologist Hiram Bingham arrived in Peru with a small team of explorers hoping to find Vilcabamba, the last Inca stronghold to fall to the Spanish.',
     panelOpenState: false,
-    link: 'https://www.history.com/topics/south-america/machu-picchu#machu-picchu-s-discovery-by-hiram-bingham'
+    link: 'https://www.history.com/topics/south-america/machu-picchu#machu-picchu-s-discovery-by-hiram-bingham',
+    image: 'https://media.wired.com/photos/5933730758b0d64bb35d56a8/master/w_2560%2Cc_limit/machu_picchu_630px.jpg'
   },
   {
     index: 0,
@@ -34,7 +37,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'The first crossword puzzle was created by Arthur Wynne, an editor at the New York World, in 1913. He called it “FUN’s Word-Cross Puzzle” and printed it in the “Fun” section of the newspaper',
     panelOpenState: false,
-    link: 'https://blog.mycrosswordmaker.com/the-history-of-crossword-puzzles/'
+    link: 'https://blog.mycrosswordmaker.com/the-history-of-crossword-puzzles/',
+    image: 'https://images2.minutemediacdn.com/image/upload/v1690906807/images/voltaxMediaLibrary/mmsport/mentalfloss/01h6ryckap3rmwfcp21e.jpg'
   },
   {
     index: 0,
@@ -43,7 +47,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'On July 20, 1969, American astronauts Neil Armstrong and Edwin "Buzz" Aldrin became the first humans ever to land on the moon. About six-and-a-half hours later, Armstrong became the first person to walk on the moon. As he took his first step, Armstrong famously said, "That\'s one small step for man, one giant leap for mankind."',
     panelOpenState: false,
-    link: 'https://www.history.com/topics/1960s/moon-landing-1969'
+    link: 'https://www.history.com/topics/1960s/moon-landing-1969',
+    image: 'https://aerospace.honeywell.com/content/dam/aerobt/en/images/learn/about-us/blogs/horizontal/AeroBT-s_285636983_moon-landing_2880x1440.jpg'
   },
   {
     index: 0,
@@ -52,7 +57,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'The Mona Lisa was painted when Leonardo was living in Florence, and it now hangs in the Louvre Museum, Paris, where it remained an object of pilgrimage in the 21st century.',
     panelOpenState: false,
-    link: 'https://www.britannica.com/topic/Mona-Lisa-painting'
+    link: 'https://www.britannica.com/topic/Mona-Lisa-painting',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/1200px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
   },
   {
     index: 0,
@@ -61,7 +67,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: ' The discovery was made based on mathematical calculations of its predicted position due to observed perturbations in the orbit of the planet Uranus. The discovery was made using a telescope since Neptune is too faint to be visible to the naked eye, owing to its great distance from the Sun.',
     panelOpenState: false,
-    link: 'https://www.nasa.gov/history/175-years-ago-astronomers-discover-neptune-the-eighth-planet/'
+    link: 'https://www.nasa.gov/history/175-years-ago-astronomers-discover-neptune-the-eighth-planet/',
+    image: 'https://media.springernature.com/lw685/springer-static/image/chp%3A10.1007%2F978-1-4614-5565-3_2/MediaObjects/270985_1_En_2_Figa_HTML.gif'
   },
   {
     index: 0,
@@ -70,7 +77,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'BCE',
     details: 'No pyramids are more celebrated than the Great Pyramids of Giza, located on a plateau on the west bank of the Nile River, on the outskirts of modern-day Cairo. The oldest and largest of the three pyramids at Giza, known as the Great Pyramid, is the only surviving structure out of the famed Seven Wonders of the Ancient World. It was built for Pharaoh Khufu (Cheops, in Greek), Sneferu’s successor and the second of the eight kings of the fourth dynasty.',
     panelOpenState: false,
-    link: 'https://www.history.com/topics/ancient-egypt/the-egyptian-pyramids#section_3'
+    link: 'https://www.history.com/topics/ancient-egypt/the-egyptian-pyramids#section_3',
+    image: 'https://lp-cms-production.imgix.net/2020-11/GettyRF_1085205362.jpg?auto=format&w=1440&h=810&fit=crop&q=75'
   },
   {
     index: 0,
@@ -79,7 +87,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'While there is evidence that the principles of telescopes were known in the late 16th century, the first telescopes were created in the Netherlands in 1608.',
     panelOpenState: false,
-    link: 'https://www.loc.gov/collections/finding-our-place-in-the-cosmos-with-carl-sagan/articles-and-essays/modeling-the-cosmos/galileo-and-the-telescope#:~:text=While%20there%20is%20evidence%20that,Jacob%20Metius%20independently%20created%20telescopes.'
+    link: 'https://www.loc.gov/collections/finding-our-place-in-the-cosmos-with-carl-sagan/articles-and-essays/modeling-the-cosmos/galileo-and-the-telescope#:~:text=While%20there%20is%20evidence%20that,Jacob%20Metius%20independently%20created%20telescopes.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/5/51/Emblemata_1624.jpg'
   },
   {
     index: 0,
@@ -88,7 +97,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'Until 1781, the known solar system consisted of six planets.  On March 13 of that year, astronomer William Herschel observed a faint object in the constellation Gemini and noted that it moved slowly relative to the background stars. First thinking it a comet, Herschel later correctly identified it as a planet and wanted to name it after the monarch of his adopted United Kingdom. But astronomers decided to maintain the tradition of naming planets after mythological figures, and the seventh planet became known as Uranus.',
     panelOpenState: false,
-    link: 'https://www.nasa.gov/history/240-years-ago-astronomer-william-herschel-identifies-uranus-as-the-seventh-planet/'
+    link: 'https://www.nasa.gov/history/240-years-ago-astronomer-william-herschel-identifies-uranus-as-the-seventh-planet/',
+    image: 'https://www.rmg.co.uk/sites/default/files/styles/full_width_1440/public/2023-07/52797919139_93262a7e5b_c.jpg?itok=MxzN2OEa'
   },
   {
     index: 0,
@@ -97,7 +107,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'The credit for the first violin is usually given to a Cremonese luthier named Andrea Amati who had made his name originally as a lute builder. He created at least two three string violins in the 1540s. He was then commissioned to build one of the first four-string violins by the wealthy Medici family in the 1550s.',
     panelOpenState: false,
-    link: 'https://www.theinstrumentplace.com/history-of-the-violin'
+    link: 'https://www.theinstrumentplace.com/history-of-the-violin',
+    image: 'https://www.violinschool.com/wp-content/uploads/2023/07/The-History-of-the-Violin-ViolinSchool-Featured-Image-1920px-x-1080px.jpg'
   },
   {
     index: 0,
@@ -106,7 +117,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'The bristle toothbrush, similar to the type used today, was not invented until 1498 in China. The bristles were actually the stiff, coarse hairs taken from the back of a hog’s neck and attached to handles made of bone or bamboo.',
     panelOpenState: false,
-    link: 'https://www.loc.gov/everyday-mysteries/technology/item/who-invented-the-toothbrush-and-when-was-it-invented/'
+    link: 'https://www.loc.gov/everyday-mysteries/technology/item/who-invented-the-toothbrush-and-when-was-it-invented/',
+    image: 'https://cdn.shopify.com/s/files/1/1233/1288/files/toothbrush_history_grande.jpg?152445251572727180'
   },
   {
     index: 0,
@@ -115,7 +127,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'Historians think China may have been the first civilization to develop a magnetic compass that could be used for navigation.',
     panelOpenState: false,
-    link: 'https://education.nationalgeographic.org/resource/compass/'
+    link: 'https://education.nationalgeographic.org/resource/compass/',
+    image: 'https://m2.dalvey.com/media/magefan_blog/history-of-the-compass-lodestone-ladle.jpg'
   },
   {
     index: 0,
@@ -124,7 +137,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'International Morse Code was used in World War II and in the Korean and Vietnam wars. It was used heavily by the shipping industry and for the safety of the seas up until the early 1990s.',
     panelOpenState: false,
-    link: 'https://www.britannica.com/topic/Morse-Code'
+    link: 'https://www.britannica.com/topic/Morse-Code',
+    image: 'https://cdn.britannica.com/18/146618-050-B398A5DB/telegraph-transmitter-Morse-Code.jpg'
   },
   {
     index: 0,
@@ -133,7 +147,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'BCE',
     details: 'The Bronze Age was the first truly transformative moment in civilization. For the first time ever, humanity’s dependency on ineffectual and less malleable materials lessened. Stone or wood were quickly replaced with the much more advanced bronze.',
     panelOpenState: false,
-    link: 'https://bronzeconnection.com/history-of-bronze/'
+    link: 'https://bronzeconnection.com/history-of-bronze/',
+    image: 'https://i.kinja-img.com/image/upload/c_fit,q_60,w_645/mp5oyneev2ioogeinhqy.jpg'
   },
   {
     index: 0,
@@ -142,7 +157,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'The first hourglass, or sand clock, is said to have been invented by a French monk called Liutprand in the 8th century AD.',
     panelOpenState: false,
-    link: 'https://www.guinnessworldrecords.com/world-records/first-hourglass'
+    link: 'https://www.guinnessworldrecords.com/world-records/first-hourglass',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Ambrogio_Lorenzetti_002-detail-Temperance.jpg/220px-Ambrogio_Lorenzetti_002-detail-Temperance.jpg'
   },
   {
     index: 0,
@@ -151,7 +167,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'The earliest encyclopedic work to have survived to modern times is the Naturalis Historia of Pliny the Elder, a Roman statesman living in the 1st century AD. He compiled a work of 37 chapters covering natural history, architecture, medicine, geography, geology, and all aspects of the world around him.',
     panelOpenState: false,
-    link: 'https://www.britannica.com/topic/encyclopaedia/History-of-encyclopaedias'
+    link: 'https://www.britannica.com/topic/encyclopaedia/History-of-encyclopaedias',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/2/26/Pliny_the_Elder%2C_Natural_History%2C_Florence%2C_Plut._82.4.jpg'
   },
   {
     index: 0,
@@ -160,7 +177,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'BCE',
     details: 'Pythagoras was a Greek philosopher, mathematician, and founder of the Pythagorean brotherhood that, although religious in nature, formulated principles that influenced the thought of Plato and Aristotle and contributed to the development of mathematics and Western rational philosophy.',
     panelOpenState: false,
-    link: 'https://www.britannica.com/science/Pythagorean-theorem'
+    link: 'https://www.britannica.com/science/Pythagorean-theorem',
+    image: 'https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL3B5dGguanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo4Mjh9fX0='
   },
   {
     index: 0,
@@ -169,7 +187,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'An American, John J Loud, received the first patent for a ballpoint pen back in 1888. Loud, a lawyer and occasional inventor, wanted an ink pen which would be able to write on rougher materials such as wood and leather as well as paper.',
     panelOpenState: false,
-    link: 'https://www.bbc.com/future/article/20201028-history-of-the-ballpoint-pen'
+    link: 'https://www.bbc.com/future/article/20201028-history-of-the-ballpoint-pen',
+    image: 'https://pbs.twimg.com/media/FDPN7upVcAEiCcj?format=jpg&name=large'
   },
   {
     index: 0,
@@ -178,7 +197,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'Celsius was professor of astronomy at Uppsala University from 1730 to 1744, and in 1740 he built the Uppsala Observatory. In 1733 Celsius published a collection of 316 observations of the aurora borealis, or northern lights.',
     panelOpenState: false,
-    link: 'https://www.britannica.com/biography/Anders-Celsius'
+    link: 'https://www.britannica.com/biography/Anders-Celsius',
+    image: 'https://assets-us-01.kc-usercontent.com/9dd25524-761a-000d-d79f-86a5086d4774/40ff1aca-98ff-4679-8b9a-8b58a8c08138/celsius3.jpg?w=800&h=600&auto=format&q=75&fit=crop'
   },
   {
     index: 0,
@@ -187,7 +207,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'The Lord of the Rings is an epic high fantasy novel written by J.R.R. Tolkien, which was later fitted as a trilogy. The story began as a sequel to Tolkien\'s earlier fantasy book The Hobbit, and soon developed into a much larger story. At publication, it was divided into The Fellowship of the Ring, The Two Towers, and The Return of the King.',
     panelOpenState: false,
-    link: 'https://www.tolkienestate.com/writing/the-lord-of-the-rings/'
+    link: 'https://www.tolkienestate.com/writing/the-lord-of-the-rings/',
+    image: 'https://www.bishopsstortfordindependent.co.uk/_media/img/7KEHDN8R2BA350IP5CUN.jpg'
   },
   {
     index: 0,
@@ -196,7 +217,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'The story was originally told by Carroll to Lorina, Alice, and Edith Liddell (the daughters of Henry George Liddell, dean of Christ Church, Oxford, where the author had studied and held a fellowship) on a picnic in July 1862. Alice asked Carroll to write out the stories for her, and in response he produced a hand-lettered collection entitled Alice’s Adventures Under Ground.',
     panelOpenState: false,
-    link: 'https://www.britannica.com/topic/Alices-Adventures-in-Wonderland'
+    link: 'https://www.britannica.com/topic/Alices-Adventures-in-Wonderland',
+    image: 'https://i0.wp.com/www.raptisrarebooks.com/images/183536/alices-adventures-in-wonderland-lewis-carroll-charles-lutwidge-dodgson-first-edition-signed.jpg?fit=1250%2C1000&ssl=1'
   },
   {
     index: 0,
@@ -205,7 +227,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'The invention of various kinds of machines was attempted in the 19th century. Most were large and cumbersome, some resembling pianos in size and shape. All were much slower to use than handwriting. Finally, in 1867, the American inventor Christopher Latham Sholes read an article in the journal Scientific American describing a new British-invented machine and was inspired to construct what became the first practical typewriter. His second model, patented on June 23, 1868, wrote at a speed far exceeding that of a pen.',
     panelOpenState: false,
-    link: 'https://www.britannica.com/technology/typewriter'
+    link: 'https://www.britannica.com/technology/typewriter',
+    image: 'https://cdn.britannica.com/60/189960-004-48EE9799/typewriter-Remington-Standard-Frederick-Douglass.jpg'
   },
   {
     index: 0,
@@ -214,7 +237,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'The first book on deaf education, published in 1620 by Juan Pablo Bonet in Madrid, included a detailed account of the use of a manual alphabet to teach deaf students to read and speak.',
     panelOpenState: false,
-    link: 'https://en.wikipedia.org/wiki/History_of_sign_language'
+    link: 'https://en.wikipedia.org/wiki/History_of_sign_language',
+    image: 'https://blogs.bl.uk/.a/6a00d8341c464853ef0240a4a97d69200b-320wi'
   },
   {
     index: 0,
@@ -223,7 +247,8 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'The first can opener was actually an American invention, patented by Ezra J. Warner on January 5, 1858. At this time, writes Connecticut History, “iron cans were just starting to be replaced by thinner steel cans.”',
     panelOpenState: false,
-    link: 'https://www.smithsonianmag.com/smart-news/why-can-opener-wasnt-invented-until-almost-50-years-after-can-180964590/'
+    link: 'https://www.smithsonianmag.com/smart-news/why-can-opener-wasnt-invented-until-almost-50-years-after-can-180964590/',
+    image: 'https://cdn.arstechnica.net/wp-content/uploads/2017/11/640px-Apriscatole_-_Musei_del_cibo_-_Pomodoro_-_048.jpg'
   },
   {
     index: 0,
@@ -232,6 +257,7 @@ export const EventDetails: Event[] = [
     dateEnding: 'AD',
     details: 'Last Supper, one of the most famous artworks in the world, painted by Leonardo da Vinci probably between 1495 and 1498 for the Dominican monastery Santa Maria delle Grazie in Milan. It depicts the dramatic scene described in several closely connected moments in the Gospels, including Matthew 26:21–28, in which Jesus declares that one of the Apostles will betray him and later institutes the Eucharist.',
     panelOpenState: false,
-    link: 'https://www.britannica.com/topic/Last-Supper-fresco-by-Leonardo-da-Vinci'
+    link: 'https://www.britannica.com/topic/Last-Supper-fresco-by-Leonardo-da-Vinci',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/%C3%9Altima_Cena_-_Da_Vinci_5.jpg/1920px-%C3%9Altima_Cena_-_Da_Vinci_5.jpg',
   }
 ];
